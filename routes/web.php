844aac26cb3622451b66a69518bffbfe267aa5e1
Shokhrukhmirzo Gulomov <mirzo.github@gmail.com>
2021-04-25 23:48:42 +0500

    completed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +14,86 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog\\index');
 });
+
+Route::get('/post/{id}', function () {
+    return view('blog\\post');
+})->name('post');
+
+Route::get('/admin', function () {
+    return view('admin\\index');
+})->name('admin.index');
+
+Route::get('/admin/create', function () {
+    return view('admin\\create');
+})->name('admin.create');
+
+Route::get('/admin/edit/{id}', function () {
+    return view('admin\\edit');
+})->name('admin.edit');
+
+Route::get('/about', function () {
+    return view('other\\about');
+})->name('about');
+
+Route::post('create',function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator){
+    $validation=$validator->make($request->all(),[
+        'title'=> 'required|min:5',
+        'content'=>'required|min:10'
+    ]);
+    if($validation->fails()){
+        return redirect()->back()->withErrors($validation);
+    }
+    return redirect()->route('admin.index')
+        ->with('info','Post created, Title: '.$request->input('title'));
+})->name('admin.create');
+
+
+Route::post('edit', function (\Illuminate\Http\Request $request ,
+                                  \Illuminate\Validation\Factory $validator){
+    $validation = $validator->make($request->all(), [
+        'title' => 'required|min:5',
+        'content' => 'required|min:10'
+    ]);
+    if ( $validation->fails()) {
+        return redirect()->back()->withErrors($validation);
+    }
+    return redirect()
+        ->route('admin.index')
+        ->with('info','Post edited, new Title: '.$request->input('title'));
+})->name('admin.update');
+
+
+Route::group([ 'prefix' => 'admin'], function() {
+    Route::get ('', [
+        'uses' => '\App\Http\Controllers\PostController@getAdminIndex',
+        'as' => 'admin.index'
+    ]);
+    Route :: get ('create ', [
+        'uses' => '\App\Http\Controllers\PostController@getAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::post('create', [
+        'uses' => '\App\Http\Controllers\PostController@postAdminCreate',
+        'as' => 'admin.create'
+    ]);
+    Route::get('edit/{ id}', [
+        'uses' => '\App\Http\Controllers\PostController@getAdminEdit',
+        'as' => 'admin.edit'
+    ]);
+    Route::post('edit',[
+        'uses' => '\App\Http\Controllers\PostController@postAdminUpdate',
+        'as' => 'admin.update'
+    ]);
+});
+
+Route::get ('/', [
+    'uses' => '\App\Http\Controllers\PostController@getIndex',
+    'as' => 'blog.index'
+]);
+Route::get('post/{ id}', [
+    'uses' => '\App\Http\Controllers\PostController@getPost',
+    'as' => 'blog.post'
+]);
+
